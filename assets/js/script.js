023ddfc7c152935bdf4c7bc936068c9aa2f1d2ff
Micah Conn <mcoxconn@gmail.com>
2021-08-08 14:44:43 -0500

    working on starting the quiz hiding start button and adding questions java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,81 @@
-let answerButton = document.getElementById("answer-btn")
+let startButton = document.getElementById("start-btn")
 let timerElement = document.getElementById("timer")
+let timerCount = document.querySelector("timer")
+let questionPool = document.getElementById("question-container")
 
 
 
 
 
-
-answerButton.addEventListener("click", startTimer)
-
+startButton.addEventListener("click", startTimer)
+startButton.addEventListener("click", startGame)
 let winSenario = false;
 let timer;
-let timeCount;
+let winCounter = 0;
+let lossCounter = 0;
 
 
 
-function startGame(){
+function startTimer(){
     console.log("started")
-}
-
-
-function nextQuestion()
-
-
-function startTimer() {
-    timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount >= 0) {
-
-            if (winSenario && timerCount > 0) {
-
-                clearInterval(timer);
-                winGame();
-            }
-        }
-
-        if (timerCount === 0) {
-
-            clearInterval(timer);
-            loseGame();
-          }
-    }, 1000);
-}
     
+    timerCount = 60
+        timer = setInterval(function() {
+            timerCount--;
+            timerElement.textContent = timerCount;
+            if (timerCount >= 0) {
+    
+                if (winSenario && timerCount > 0) {
+    
+                    clearInterval(timer);
+                    // winGame();
+                }
+            }
+    
+            if (timerCount === 0) {
+    
+                clearInterval(timer);
+                // loseGame();
+              }
+        }, 1000);
+}
+
+function startGame() {
+    startButton.classList.add("hide")
+    questionPool.classList.remove("hide")
+
+}
+
+
+
+
+
+
+// function winGame() {
+//     winCounter++
+//     setWins()
+// }
+
+// function loseGame() {
+//     lossCounter++
+//     setLosses()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function nextQuestion()
+
